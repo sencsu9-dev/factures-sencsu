829c318f3a4d020db5acdec59a1c959665ee7832
sencsu9-dev <sencsu9@gmail.com>
2026-07-28 11:08:57 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // ══════════════════════════════════════════════════════════════════════════
 
 // Changer la version a chaque mise a jour pour forcer le rafraichissement du cache
-const CACHE_VERSION = "sencsu-v9";
+const CACHE_VERSION = "sencsu-v10";
 
 // Fichiers a mettre en cache pour un fonctionnement 100% hors ligne
 const FICHIERS_CACHE = [
